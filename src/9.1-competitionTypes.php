6262228f4f9a0a05c9e39625eee04005181d80ff
addshore <addshorewiki@gmail.com>
2015-06-12 12:19:37 +0100

    Add competition types to the DB

---
+++ src/9.1-competitionTypes.php
@@ -0,0 +1,31 @@
+<?php
+require_once  dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once  __DIR__ . DIRECTORY_SEPARATOR . 'Migration.php';
+$to = Migration::getNewDb();
+$toTable = 'swa_competition_type';
+$to->beginTransaction();
+$toQueryBuilder = $to->createQueryBuilder();
+$toQueryBuilder->insert( Migration::getToTable( $toTable ) );
+
+$types = array(
+    'Beginner Race',
+    'Intermediate Race',
+    'Advanced Race',
+    'Freestyle',
+    'Wave',
+);
+
+foreach( $types as $type ) {
+    $toQueryBuilder->values( array(
+        'name' => $to->quote( $type, PDO::PARAM_STR ),
+    ) );
+    $insertResult = $to->query( $toQueryBuilder->getSQL() );
+}
+try{
+    $to->commit();
+} catch(Exception $e) {
+    $to->rollback();
+    throw $e;
+}
+
+echo "Done!";
